57e5cbe596b1cb506d79963ebba46a10b24679e2
Guti <gustavo.costanti.lara@gmail.com>
2021-05-07 01:58:19 -0300

    09 - Front-Web Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
   );
 }
 
