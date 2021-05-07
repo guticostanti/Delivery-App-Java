7e02bc017c4d48e8dff9e9ad1bbf02c6f4beba81
Guti <gustavo.costanti.lara@gmail.com>
2021-05-06 21:22:50 -0300

    05 - POST /orders

--- backend/src/main/java/com/guticostanti/delivery/services/OrderService.java
+++ backend/src/main/java/com/guticostanti/delivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.guticostanti.delivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.guticostanti.delivery.dto.OrderDTO;
+import com.guticostanti.delivery.dto.ProductDTO;
 import com.guticostanti.delivery.entities.Order;
+import com.guticostanti.delivery.entities.OrderStatus;
+import com.guticostanti.delivery.entities.Product;
 import com.guticostanti.delivery.repositories.OrderRepository;
+import com.guticostanti.delivery.repositories.ProductRepository;
 
 
 @Service
@@ -18,9 +23,24 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
